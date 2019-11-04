c925ced771e1c2846ab12ff55458e920405e3407
abeshi1019 <ibanez1111.music@gmail.com>
2019-11-04 12:39:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,30 @@ import React from 'react';
 //   return dom;
 // }
 
-function App() {
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}} />;
+//     </React.Fragment>
+//   ) 
+// }
+
+const App =()=> {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />;
-    </React.Fragment>
-  ) 
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
+
+const Cat =()=> {
+  return <div>Meow!</div>
+}
+
 
 
 export default App;
