cb3c0f086c1d4ca161317dfc4e697ff4fd66439d
abeshi1019 <ibanez1111.music@gmail.com>
2019-11-04 12:21:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 
+// function App() {
+//   const greeting = "Hi!Tom"
+//   const dom = <h1 className="foo">{greeting}</h1>
+//   return dom;
+// }
+
 function App() {
   return (
-    <h1>
-        Hello React
-    </h1>
-  );
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}} />;
+    </React.Fragment>
+  ) 
 }
 
 
