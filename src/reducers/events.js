4155755a7be87ef2357e2c7ebf43a66c1a75fe25
abeshi1019 <ibanez1111.music@gmail.com>
2019-11-08 16:24:46 +0900

    [fix] typo
    actionでtype:READ_EVENTSとしているので、action.typesでは拾えない。

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -2,11 +2,9 @@ import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
 export default (events = {}, action) => {
-  switch (action.types) {
+  switch (action.type) {
     case READ_EVENTS:
-      console.log(action)
-      console.log(_.mapKeys(action.response.data, 'id'))
-      return events
+      return _.mapKeys(action.response.data, 'id')
     default:
       return events
   }
