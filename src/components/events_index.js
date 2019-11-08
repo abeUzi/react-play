101c4bc252a8e47cc75bf55ef4c6fc5a9819d59c
abeshi1019 <ibanez1111.music@gmail.com>
2019-11-08 16:24:56 +0900

    [update] html

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,30 +1,45 @@
-import React,{ Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import _ from 'lodash'
 
-import { readEvents } from '../actions';
-
-
-
+import { readEvents } from '../actions'
 
 class EventsIndex extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.readEvents()
   }
 
-  render (){
-    const props = this.props
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
 
+  render() {
     return (
-      <React.Fragment>
-        <div>{console.log(props.events)}</div>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {this.renderEvents()}
+        </tbody>
+      </table>
     )
   }
 }
 
-// Stateとアクションをコンポーネントに結びつける
-const mapStateToProps = state => ({ events:state.events })
-const mapDispatchToProps = ({ readEvents })
+const mapStateToProps = state => ({ events: state.events })
 
+const mapDispatchToProps = ({ readEvents })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
